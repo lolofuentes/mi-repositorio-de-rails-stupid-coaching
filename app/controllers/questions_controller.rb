3d2a35bae4bcf82aed38ca39b0585b7294f03ca4
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-10 20:00:40 -0400

    realicé views, controllers y style entre otras cosas usando el desafío

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,13 @@ class QuestionsController < ApplicationController
     end
 
     def answer
+        @pregunta = params[:pregunta]
+        if @pregunta == "I am going to work"
+            @respuesta = "great!"
+        elsif @pregunta.end_with?("?")
+            @respuesta = "Silly question, get dressed and go to work!"
+        else
+            @respuesta = "I don't care, get dressed and go to work!"
+        end 
     end
-
 end
